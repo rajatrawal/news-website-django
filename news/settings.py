75b5d430fcf86aa1eb1816484c261304c7b2bd25
Rajat <rwlrajat@gmail.com>
2023-12-05 15:45:29 +0530

    initial commit

--- news/settings.py
+++ news/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://news-app-django.onrender.com/']
+ALLOWED_HOSTS = ['news-app-django.onrender.com']
 
 
 # Application definition
